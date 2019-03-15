6e3ed155aa0f8be19a6eac1e4b692d24829472dd
Trực Gia Phú - 1512413 <1512413@student.hcmus.edu.vn>
2019-03-15 10:29:59 +0700

    make it better

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ app.get('/', (req, res)=>{
     res.json('Hi buddy');
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000 ;
 
-app.listen(PORT, ()=>{
-    console.log(`Magic happened on port ${PORT}`);
-});
+app.listen(PORT);
