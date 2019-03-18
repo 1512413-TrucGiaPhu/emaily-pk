eb48dcfbb14da2e2aef08c5df247c106be729b98
Trực Gia Phú - 1512413 <1512413@student.hcmus.edu.vn>
2019-03-18 22:06:17 +0700

    changed

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.deserializeUser((id, done)=>{
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleCLientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, email, done) => {
     User.findOne({ googleId: email.id })
         .then((existingUser) => {
